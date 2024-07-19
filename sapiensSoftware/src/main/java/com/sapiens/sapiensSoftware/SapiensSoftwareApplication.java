9e370e6e65a00cb2ae45f2e32eb3bbdcbeee828d
Kaio Fernandes <kaiofs122@gmail.com>
2024-07-19 07:57:44 -0300

    Configurando o Springboot

---
+++ sapiensSoftware/src/main/java/com/sapiens/sapiensSoftware/SapiensSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.sapiens.sapiensSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapiensSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapiensSoftwareApplication.class, args);
+	}
+
+}
---
+++ sapiensSoftware/src/test/java/com/sapiens/sapiensSoftware/SapiensSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapiens.sapiensSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapiensSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
