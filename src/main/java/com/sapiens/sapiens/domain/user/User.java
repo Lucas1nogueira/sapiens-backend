0c19cf067e428909f484d0b7b9a1f4a4104cd6bb
Jonas Dias <zdias1413@gmail.com>
2024-07-29 20:24:55 -0300

    fix: add JsonIgnore to GrantedAuthority on User class
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>

--- src/main/java/com/sapiens/sapiens/domain/user/User.java
+++ src/main/java/com/sapiens/sapiens/domain/user/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -38,6 +39,7 @@ public class User implements UserDetails {
         this.firstLogin = true;
     }
 
+    @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
