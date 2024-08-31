3c4f45cf4f71701284bee729c5dbc5f1fc168e22
Jonas Dias <zdias1413@gmail.com>
2024-08-31 16:12:55 -0300

    feat: add admin repository and model
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>
    Co-authored-by: Ellen Cristina <EllenCristina10@users.noreply.github.com>

--- src/main/java/com/sapiens/sapiens/controllers/AdminController.java
+++ src/main/java/com/sapiens/sapiens/controllers/AdminController.java
@@ -2,8 +2,12 @@ package com.sapiens.sapiens.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sapiens.sapiens.domain.admin.Admin;
 import com.sapiens.sapiens.services.AdminService;
 import lombok.AllArgsConstructor;
 
@@ -17,5 +21,10 @@ public class AdminController {
     @GetMapping("/all")
     public ResponseEntity<?> findAll() {
         return adminService.findAll();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Admin admin) {
+        return adminService.update(admin);
     }
 }
