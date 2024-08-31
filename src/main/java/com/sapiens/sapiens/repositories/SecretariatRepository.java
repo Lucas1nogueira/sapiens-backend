b9154f66dc748f3da5b0a50e7ac1a845c9db0dbd
Jonas Dias <zdias1413@gmail.com>
2024-08-31 16:12:05 -0300

    feat: add secretariat CRUD
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>
    Co-authored-by: Ellen Cristina <EllenCristina10@users.noreply.github.com>

---
+++ src/main/java/com/sapiens/sapiens/repositories/SecretariatRepository.java
@@ -0,0 +1,15 @@
+package com.sapiens.sapiens.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.sapiens.sapiens.domain.secretariat.Secretariat;
+import java.util.Optional;
+
+@Repository
+public interface SecretariatRepository extends JpaRepository<Secretariat, Long> {
+
+    Optional<Secretariat> findByName(String name);
+
+    Optional<Secretariat> findBySchoolsId(Long id);
+
+}
---
+++ src/main/java/com/sapiens/sapiens/services/SecretariatService.java
@@ -0,0 +1,39 @@
+package com.sapiens.sapiens.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.sapiens.sapiens.domain.secretariat.Secretariat;
+import com.sapiens.sapiens.infra.exceptions.BusinessException;
+import com.sapiens.sapiens.repositories.SecretariatRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class SecretariatService {
+
+    private final SecretariatRepository secretariatRepository;
+
+    public ResponseEntity<?> save(Secretariat secretariat) {
+        if (secretariatRepository.existsById(secretariat.getId())) {
+            throw new BusinessException("Secretaria já registrada.");
+        }
+
+        return ResponseEntity.ok().body(secretariatRepository.save(secretariat));
+    }
+
+    public ResponseEntity<?> update(Secretariat secretariat) {
+        return ResponseEntity.ok().body(secretariatRepository.save(secretariat));
+    }
+
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok().body(secretariatRepository.findAll());
+    }
+
+    public ResponseEntity<?> findByName(String name) {
+        return ResponseEntity.ok().body(secretariatRepository.findByName(name));
+    }
+
+    public ResponseEntity<?> findBySchoolsId(Long id) {
+        return ResponseEntity.ok().body(secretariatRepository.findBySchoolsId(id));
+    }
+}
