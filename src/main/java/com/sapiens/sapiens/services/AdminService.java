6c369a92201f51a03e2e4553a990fd8d490a5b05
Jonas Dias <zdias1413@gmail.com>
2024-08-31 16:12:55 -0300

    feat: add admin repository and model
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>
    Co-authored-by: Ellen Cristina <EllenCristina10@users.noreply.github.com>

--- src/main/java/com/sapiens/sapiens/services/AdminService.java
+++ src/main/java/com/sapiens/sapiens/services/AdminService.java
@@ -2,6 +2,9 @@ package com.sapiens.sapiens.services;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.sapiens.sapiens.domain.admin.Admin;
+import com.sapiens.sapiens.repositories.AdminRepository;
 import com.sapiens.sapiens.repositories.AuthRepository;
 import lombok.AllArgsConstructor;
 
@@ -10,8 +13,13 @@ import lombok.AllArgsConstructor;
 public class AdminService {
 
     private final AuthRepository authRepository;
+    private final AdminRepository adminRepository;
 
     public ResponseEntity<?> findAll() {
         return ResponseEntity.ok().body(authRepository.findAll());
+    }
+
+    public ResponseEntity<?> update(Admin admin) {
+        return ResponseEntity.ok().body(adminRepository.save(admin));
     }
 }
