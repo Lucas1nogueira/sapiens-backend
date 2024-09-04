16beb105eea5eb6072e24fea3b6a425234143016
Jonas Dias <zdias1413@gmail.com>
2024-09-04 11:58:06 -0300

    build: setup docker

--- src/test/java/com/sapiens/sapiens/SapiensApplicationTests.java
+++ src/test/java/com/sapiens/sapiens/SapiensApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sapiens.sapiensSoftware;
+package com.sapiens.sapiens;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SapiensSoftwareApplicationTests {
+class SapiensApplicationTests {
 
 	@Test
 	void contextLoads() {
